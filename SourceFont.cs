ee23d38bc574717fd904264e76ef22ee6af67301
Evgenii Pavlyuk <epavlyuk@sgfwest.com>
2025-04-22 20:50:37 +0400

    Fix image stretching issue

--- SourceFont.cs
+++ SourceFont.cs
@@ -163,13 +163,11 @@ namespace CLIBSTool
 
         private Bitmap CropImage(Bitmap source, Rectangle section)
         {
-            // An empty bitmap which will hold the cropped image
             Bitmap bmp = new Bitmap(section.Width, section.Height);
             Graphics g = Graphics.FromImage(bmp);
 
-            // Draw the given area (section) of the source image
-            // at location 0,0 on the empty bitmap (bmp)
-            g.DrawImage(source, 0, 0, section, GraphicsUnit.Pixel);
+            Rectangle destRec = new Rectangle(0, 0, section.Width, section.Height);
+            g.DrawImage(source, destRec, section, GraphicsUnit.Pixel);
 
             return bmp;
         }
